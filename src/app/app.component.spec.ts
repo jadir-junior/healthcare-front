d3562f837dfbcbdf7a2735bd35a4143eef7c4b75
Jadir J. S. Junior <jadirjsjunior@gmail.com>
2022-08-19 15:04:22 -0300

    :wrench: config test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,18 +1,13 @@
-import { TestBed } from '@angular/core/testing'
-import { RouterTestingModule } from '@angular/router/testing'
 import { AppComponent } from './app.component'
+import { render } from '@testing-library/angular'
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [AppComponent],
-    }).compileComponents()
-  })
+  const setup = async () => {
+    return render(AppComponent)
+  }
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent)
-    const app = fixture.componentInstance
-    expect(app).toBeTruthy()
+  it('should create the container app', async () => {
+    const { container } = await setup()
+    expect(container).toBeTruthy()
   })
 })
