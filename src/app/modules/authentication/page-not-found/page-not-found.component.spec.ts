d02257a86adf315d0e7ebe1fb2f2bf398080bc91
Jadir J. S. Junior <jadirjsjunior@gmail.com>
2022-08-26 14:08:38 -0300

    :sparkles: create page not found

--- src/app/modules/authentication/page-not-found/page-not-found.component.spec.ts
+++ src/app/modules/authentication/page-not-found/page-not-found.component.spec.ts
@@ -1,13 +1,23 @@
+import { render, screen } from '@testing-library/angular'
+
+import { ButtonModule } from 'src/app/components/button/button.module'
 import { PageNotFoundComponent } from './page-not-found.component'
-import { render } from '@testing-library/angular'
 
 describe('PageNotFoundComponent', () => {
   const setup = async () => {
-    return render(PageNotFoundComponent)
+    return render(PageNotFoundComponent, {
+      imports: [ButtonModule],
+    })
   }
 
   it('should create a page not found', async () => {
     const { container } = await setup()
+
     expect(container).toBeInTheDocument()
+    expect(screen.getByText('404')).toBeInTheDocument()
+    expect(
+      screen.getByText(`Oops. The page you were looking for doesn't exist.`)
+    ).toBeInTheDocument()
+    expect(screen.getByRole('button', { name: /back to home/i })).toBeInTheDocument()
   })
 })
