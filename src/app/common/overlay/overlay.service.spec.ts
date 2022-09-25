99b171086a5351ba577d81d0c5b5a8aaa0e4deef
Jadir Junior <jadir.junior@gmail.com>
2022-09-25 12:14:34 -0300

    fix styles code

--- src/app/common/overlay/overlay.service.spec.ts
+++ src/app/common/overlay/overlay.service.spec.ts
@@ -1,16 +1,16 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing'
 
-import { OverlayService } from './overlay.service';
+import { OverlayService } from './overlay.service'
 
 describe('OverlayService', () => {
-  let service: OverlayService;
+  let service: OverlayService
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(OverlayService);
-  });
+    TestBed.configureTestingModule({})
+    service = TestBed.inject(OverlayService)
+  })
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+    expect(service).toBeTruthy()
+  })
+})
