f7b292f1c3df42a301d82f197eb3283a0f8a51ff
Jadir Junior <jadir.junior@gmail.com>
2022-09-26 13:57:40 -0300

    search

--- src/app/modules/home/home.component.spec.ts
+++ src/app/modules/home/home.component.spec.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './home.component'
 import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { LogoModule } from './../../components/logo/logo.module'
 import { MenuModule } from './../../components/menu/menu.module'
+import { SearchModule } from './../../components/search/search.module'
 import { SidebarModule } from './../../components/sidebar/sidebar.module'
 import { TemplateModule } from './../../directives/template/template.module'
 import { ToolbarModule } from '../../components/toolbar/toolbar.module'
@@ -23,6 +24,7 @@ describe('HomeComponent', () => {
         AvatarModule,
         ToolbarModule,
         TemplateModule,
+        SearchModule,
       ],
     })
   }
