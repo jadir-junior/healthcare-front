598a949e0b66b8015387cf4f974ca4194612aa8f
Jadir Junior <jadir.junior@gmail.com>
2022-10-01 10:02:03 -0300

    doc scroll table

--- src/app/modules/docs/table/page-scroll/page-scroll.component.ts
+++ src/app/modules/docs/table/page-scroll/page-scroll.component.ts
@@ -6,84 +6,124 @@ import { IColumn } from './../../../../components/table/table.component'
 @Component({
   selector: 'app-page-scroll',
   template: `
-    <hc-card>
-      <h6 style="margin-bottom: 1rem">Vertical</h6>
-      <hc-table
-        [value]="products"
-        [columns]="cols"
-        [scrollable]="true"
-        scrollDirection="horizontal"
-      >
-        <ng-template hcTemplate="header" let-columns>
-          <tr>
-            <th *ngFor="let column of columns" style="width: 200px">
-              {{ column.header }}
-            </th>
-          </tr>
-        </ng-template>
-        <ng-template hcTemplate="body" let-product let-columns="columns">
-          <tr>
-            <td *ngFor="let column of columns" style="width: 200px">
-              {{ product[column.field] }}
-            </td>
-          </tr>
-        </ng-template>
-      </hc-table>
-    </hc-card>
+    <div class="hc-docs-title">
+      <h2>DataTable Scroll</h2>
+      <p class="body1">
+        Scrollable é habilitado para rolar na tabela em vertical, hotizontal ou os dois
+        "<i class="hc-docs-value">both</i>"
+      </p>
+    </div>
+    <div class="hc-docs-components">
+      <hc-card>
+        <h6 style="margin-bottom: 1rem">Horizontal</h6>
+        <hc-table
+          [value]="products"
+          [columns]="cols"
+          [scrollable]="true"
+          scrollDirection="horizontal"
+        >
+          <ng-template hcTemplate="header" let-columns>
+            <tr>
+              <th *ngFor="let column of columns" style="width: 200px">
+                {{ column.header }}
+              </th>
+            </tr>
+          </ng-template>
+          <ng-template hcTemplate="body" let-product let-columns="columns">
+            <tr>
+              <td *ngFor="let column of columns" style="width: 200px">
+                {{ product[column.field] }}
+              </td>
+            </tr>
+          </ng-template>
+        </hc-table>
+      </hc-card>
 
-    <hc-card [style]="{ 'margin-top': '1rem' }">
-      <h6 style="margin-bottom: 1rem">Horizontal</h6>
-      <hc-table
-        [value]="products"
-        [columns]="cols"
-        [scrollable]="true"
-        scrollDirection="vertical"
-        responsiveLayout="scroll"
-        scrollHeight="480px"
-      >
-        <ng-template hcTemplate="header" let-columns>
-          <tr>
-            <th *ngFor="let column of columns">
-              {{ column.header }}
-            </th>
-          </tr>
-        </ng-template>
-        <ng-template hcTemplate="body" let-product let-columns="columns">
-          <tr>
-            <td *ngFor="let column of columns">
-              {{ product[column.field] }}
-            </td>
-          </tr>
-        </ng-template>
-      </hc-table>
-    </hc-card>
+      <hc-card [style]="{ 'margin-top': '1rem' }">
+        <h6 style="margin-bottom: 1rem">Vertical</h6>
+        <hc-table
+          [value]="products"
+          [columns]="cols"
+          [scrollable]="true"
+          scrollDirection="vertical"
+          responsiveLayout="scroll"
+          scrollHeight="480px"
+        >
+          <ng-template hcTemplate="header" let-columns>
+            <tr>
+              <th *ngFor="let column of columns">
+                {{ column.header }}
+              </th>
+            </tr>
+          </ng-template>
+          <ng-template hcTemplate="body" let-product let-columns="columns">
+            <tr>
+              <td *ngFor="let column of columns">
+                {{ product[column.field] }}
+              </td>
+            </tr>
+          </ng-template>
+        </hc-table>
+      </hc-card>
 
-    <hc-card [style]="{ 'margin-top': '1rem' }">
-      <h6 style="margin-bottom: 1rem">Vertial e Horizontal</h6>
-      <hc-table
-        [value]="products"
-        [columns]="cols"
-        [scrollable]="true"
-        scrollDirection="both"
-        responsiveLayout="scroll"
-        scrollHeight="480px"
-      >
+      <hc-card [style]="{ 'margin-top': '1rem' }">
+        <h6 style="margin-bottom: 1rem">Vertical e Horizontal</h6>
+        <hc-table
+          [value]="products"
+          [columns]="cols"
+          [scrollable]="true"
+          scrollDirection="both"
+          responsiveLayout="scroll"
+          scrollHeight="480px"
+        >
+          <ng-template hcTemplate="header" let-columns>
+            <tr>
+              <th *ngFor="let column of columns">
+                {{ column.header }}
+              </th>
+            </tr>
+          </ng-template>
+          <ng-template hcTemplate="body" let-product let-columns="columns">
+            <tr>
+              <td *ngFor="let column of columns">
+                {{ product[column.field] }}
+              </td>
+            </tr>
+          </ng-template>
+        </hc-table>
+      </hc-card>
+    </div>
+    <h4 class="hc-docs-section">Documentação</h4>
+
+    <div class="hc-docs-section">
+      <h5>Import</h5>
+      <markdown src="/assets/docs/table/common/import.ts"></markdown>
+    </div>
+    <div class="hc-docs-section">
+      <h5>Codigo:</h5>
+      <p class="body1">
+        Use o atributo <i class="hc-docs-value">[scrollable]</i> para todos os tipo de
+        direção e o <i class="hc-docs-value">[sortDirection]</i> para direção desejada
+      </p>
+      <markdown src="/assets/docs/table/scroll/scroll.html"></markdown>
+      <markdown src="/assets/docs/table/scroll/component.ts"></markdown>
+    </div>
+
+    <div class="hc-docs-section">
+      <h5>Propriedades:</h5>
+      <hc-table [value]="props" [columns]="colsProps">
         <ng-template hcTemplate="header" let-columns>
           <tr>
-            <th *ngFor="let column of columns">
-              {{ column.header }}
-            </th>
+            <th *ngFor="let column of columns">{{ column.header }}</th>
           </tr>
         </ng-template>
-        <ng-template hcTemplate="body" let-product let-columns="columns">
+        <ng-template hcTemplate="body" let-prop let-columns="columns">
           <tr>
-            <td *ngFor="let column of columns">
-              {{ product[column.field] }}
-            </td>
+            <td *ngFor="let col of columns">{{ prop[col.field] }}</td>
           </tr>
         </ng-template>
       </hc-table>
-    </hc-card>
+    </div>
   `,
   styles: [],
 })
@@ -99,6 +139,28 @@ export class PageScrollComponent implements OnInit {
     { header: 'Quantity', field: 'quantity' },
     { header: 'Inventory Status', field: 'inventoryStatus' },
     { header: 'Rating', field: 'rating' },
+  ]
+
+  colsProps: IColumn[] = [
+    { header: 'Name', field: 'name' },
+    { header: 'Type', field: 'type' },
+    { header: 'Default', field: 'default' },
+    { header: 'Description', field: 'description' },
+  ]
+
+  props = [
+    {
+      name: 'scrollable',
+      type: 'boolean',
+      default: 'false',
+      description: 'Para tornar a table scrollable',
+    },
+    {
+      name: 'scrollDirection',
+      type: 'vertical | horizontal | both',
+      default: 'both',
+      description: 'Escholhe a direção do scroll',
+    },
   ]
 
   constructor(private productsService: ProductsService) {}
