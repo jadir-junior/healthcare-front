45a954322cd5b9fa3cbdce75623faecc711f7c82
Jadir Junior <jadir.junior@gmail.com>
2022-10-04 14:26:11 -0300

    add page select

--- src/app/modules/docs/form/form.module.ts
+++ src/app/modules/docs/form/form.module.ts
@@ -8,9 +8,10 @@ import { InputModule } from 'src/app/components/input/input.module'
 import { NgModule } from '@angular/core'
 import { PageCheckboxComponent } from './page-checkbox/page-checkbox.component'
 import { PageInputTextComponent } from './page-input-text/page-input-text.component'
+import { PageSelectComponent } from './page-select/page-select.component'
 
 @NgModule({
-  declarations: [PageInputTextComponent, PageCheckboxComponent],
+  declarations: [PageInputTextComponent, PageCheckboxComponent, PageSelectComponent],
   imports: [
     CommonModule,
     FormRoutingModule,
---
+++ src/app/modules/docs/form/page-select/page-select.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-page-select',
+  template: ` <p>page-select works!</p> `,
+  styles: [],
+})
+export class PageSelectComponent {}
