d1d53131eca32b8421a4abe83ce8d3521f344460
Jadir Junior <jadir.junior@gmail.com>
2022-10-04 14:29:14 -0300

    create a select component

---
+++ src/app/components/select/select.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'hc-select',
+  template: ` <p>select works!</p> `,
+  styles: [],
+})
+export class SelectComponent {}
---
+++ src/app/components/select/select.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common'
+import { NgModule } from '@angular/core'
+import { SelectComponent } from './select.component'
+
+@NgModule({
+  declarations: [SelectComponent],
+  imports: [CommonModule],
+  exports: [SelectComponent],
+})
+export class SelectModule {}
