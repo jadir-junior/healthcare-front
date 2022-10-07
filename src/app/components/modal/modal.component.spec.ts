ffd96623bb5cdfd341efedd036fa8161297bb6ff
Jadir Junior <jadir.junior@gmail.com>
2022-10-07 13:28:49 -0300

    create modal

--- src/app/components/modal/modal.component.spec.ts
+++ src/app/components/modal/modal.component.spec.ts
@@ -1,9 +1,69 @@
+import { render, screen } from '@testing-library/angular'
+
+import { ButtonModule } from 'src/app/components/button/button.module'
 import { ModalComponent } from './modal.component'
-import { render } from '@testing-library/angular'
+import { ModalModule } from './modal.module'
+import { TemplateModule } from 'src/app/directives/template/template.module'
+import userEvent from '@testing-library/user-event'
 
 describe('ModalComponent', () => {
   it('create a modal component', async () => {
     const { container } = await render(ModalComponent)
     expect(container).toBeInTheDocument()
+  })
+
+  it('show the modal', async () => {
+    await render(
+      `<div>
+      <hc-button (click)="display=true">Show</hc-button>
+      <hc-modal id="modal-test" header="title" [(visible)]="display" [modal]="true">
+        content of modal
+        <ng-template hcTemplate="footer">
+          <hc-button (click)="display=false">Ok</hc-button>
+        </ng-template>
+      </hc-modal>
+    </div>`,
+      {
+        imports: [ButtonModule, ModalModule, TemplateModule],
+        componentProperties: {
+          display: false,
+        },
+      }
+    )
+
+    expect(screen.queryByRole('modal')).not.toBeInTheDocument()
+
+    await userEvent.click(screen.getByRole('button', { name: /show/i }))
+
+    expect(screen.getByRole('modal')).toBeInTheDocument()
+    expect(screen.getByText(/title/i)).toBeInTheDocument()
+    expect(screen.getByText(/content of modal/i)).toBeInTheDocument()
+    expect(screen.getByRole('button', { name: /ok/i })).toBeInTheDocument()
+    expect(screen.getByLabelText(/close/i)).toBeInTheDocument()
+  })
+
+  it('close the modal', async () => {
+    await render(
+      `<div>
+      <hc-button (click)="display=true">Show</hc-button>
+      <hc-modal id="modal-test" header="title" [(visible)]="display" [modal]="true">
+        content of modal
+        <ng-template hcTemplate="footer">
+          <hc-button (click)="display=false">Ok</hc-button>
+        </ng-template>
+      </hc-modal>
+    </div>`,
+      {
+        imports: [ButtonModule, ModalModule, TemplateModule],
+        componentProperties: {
+          display: false,
+        },
+      }
+    )
+
+    await userEvent.click(screen.getByRole('button', { name: /show/i }))
+    await userEvent.click(screen.getByLabelText(/close/i))
+
+    expect(screen.queryByRole('modal')).not.toBeInTheDocument()
   })
 })
--- src/app/modules/docs/overlay/overlay.module.ts
+++ src/app/modules/docs/overlay/overlay.module.ts
@@ -1,10 +1,21 @@
+import { ButtonModule } from 'src/app/components/button/button.module'
+import { CardModule } from '../../../components/card/card.module'
 import { CommonModule } from '@angular/common'
+import { ModalModule } from '../../../components/modal/modal.module'
 import { NgModule } from '@angular/core'
 import { OverlayRoutingModule } from './overlay-routing.module'
 import { PageModalComponent } from './page-modal/page-modal.component'
+import { TemplateModule } from 'src/app/directives/template/template.module'
 
 @NgModule({
   declarations: [PageModalComponent],
-  imports: [CommonModule, OverlayRoutingModule],
+  imports: [
+    CommonModule,
+    OverlayRoutingModule,
+    CardModule,
+    ButtonModule,
+    ModalModule,
+    TemplateModule,
+  ],
 })
 export class OverlayModule {}
