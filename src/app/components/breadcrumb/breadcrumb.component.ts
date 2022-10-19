b106b5e28d18d9900b7d4ad63340b39685999dd1
Jadir Junior <jadir.junior@gmail.com>
2022-10-19 15:08:18 -0300

    create module breadcrumb

---
+++ src/app/components/breadcrumb/breadcrumb.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'hc-breadcrumb',
+  template: ` <div>breadcrumb</div> `,
+  styles: [],
+})
+export class BreadcrumbComponent {}
---
+++ src/app/components/breadcrumb/breadcrumb.module.ts
@@ -0,0 +1,10 @@
+import { BreadcrumbComponent } from './breadcrumb.component'
+import { CommonModule } from '@angular/common'
+import { NgModule } from '@angular/core'
+
+@NgModule({
+  declarations: [BreadcrumbComponent],
+  imports: [CommonModule],
+  exports: [BreadcrumbComponent],
+})
+export class BreadcrumbModule {}
